b831900c9b9c5e950b2a9f710273757c2b8e5fbb
Quentin Vecchio <quentin.vecchio@insa-lyon.fr>
2017-04-11 11:38:38 +0200

    Creation d'un module location

--- location.js
+++ location.js
@@ -1,13 +1,16 @@
-function addQuickReplyLocation(messages) {
-	messages.push({
-	    type: 'quickReplies',
-	    content: {
-	    	title: "J'ai besoin de votre localisation",
-		    buttons: [
-		      {
-		        type: "location"
-		      },
-		    ]
-	    },
-	 });
-}
+'use strict';
+
+module.exports = {
+	addQuickReplyLocation: function(messages) {
+	    messages.push({
+		    type: 'quickReplies',
+		    content: {
+		    	title: "J'ai besoin de votre localisation",
+			    buttons: [
+			      {
+			        type: "location"
+			      },
+			    ]
+		    },
+		});
+  	}
