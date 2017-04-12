ad738befc43ac847c7c935236ff6ca371734b19b
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-04-12 08:42:45 +0200

    data test and little script to clean GrandLyon open data

--- clean.py
+++ clean.py
@@ -13,24 +13,24 @@ json_file='data/POI_touristique.json'
 
 json_data=open(json_file)
 data = json.load(json_data)
-#pprint(data)
 
-# for f in data['features']: 
-# 	if f['properties']['type'] != 'DEGUSTATION' :
-		#print('property.type: ' + f['properties']['type'])
-		
-
-out = data
 i = 0
 while i != len(data['features']) :
+	del data['features'][i]['properties']['id_sitra1']
+	del data['features'][i]['properties']['fax']
+	del data['features'][i]['properties']['telephonefax']
+	del data['features'][i]['properties']['gid']
+	del data['features'][i]['properties']['date_creation']
+	del data['features'][i]['properties']['last_update']
+	del data['features'][i]['properties']['last_update_fme']
 	if data['features'][i]['properties']['type'] != 'PATRIMOINE_CULTUREL':
-		# print(data['features'][i]['properties']['type'])
 		data['features'].pop(i)
 	else :
 		i = i+1
 
-with open('data/POI_touristisque_filtered.json', 'w') as f:
-     json.dump(data, f)
+with open('data/POI_touristique_filtered.json', 'w') as f:
+	f.write(json.dumps(data, indent=4, sort_keys=True))
+    # json.dump(data, f)
 
 
 json_data.close()
