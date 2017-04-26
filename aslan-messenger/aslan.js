236550475380b2b81694561b7793658ca3fe00d5
Quentin Vecchio <quentin.vecchio@insa-lyon.fr>
2017-04-26 13:42:01 +0200

    Modification du retour de la fonction post message

--- aslan-messenger/aslan.js
+++ aslan-messenger/aslan.js
@@ -130,7 +130,7 @@ function receive(req, res) {
 				    	if(data.length == 0) {
 				    		res.status(500).send(JSON.stringify({error: error.toString()}));
 				    	} else {
-				    		res.status(200).send(JSON.stringify({success:"success"}));
+				    		res.status(200).send(JSON.stringify(data));
 				    	}
 	    			});
 	    		} else {
