cbccf0f11bcbcae375680fa767739e1b43cd101e
Quentin Vecchio <quentin.vecchio@insa-lyon.fr>
2017-05-02 22:14:29 +0200

    fix bug

--- core/core.js
+++ core/core.js
@@ -158,7 +158,7 @@ function askLocation() {
         channel: messageReceived.channel,
         text: "J'ai besoin de votre localisation"
     }
-    sendMessage([message]);
+    sendMessages([message]);
 }
 
 /*
