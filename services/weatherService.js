75cf847c166d61cf7a788196980d66d7be8b8334
Papnix <papnix@gmail.com>
2017-05-02 10:55:02 +0200

    Ajout weather service

---
+++ services/weatherService.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var request = require("request");
+
+var _PremiumApiBaseURL = 'https://api.worldweatheronline.com/premium/v1/';
+var _PremiumApiKey = process.env.WEATHER_API_KEY;
+
+// -------------------------------------------
+const format = "json";
+const language = "fr"
+
+// Le format de la date doit etre YYYY-MM-DD, genre 2017-05-02
+function JSONP_LocalWeather(city, date, callback) {
+    var url = _PremiumApiBaseURL + 'weather.ashx?q=' + city +
+    		 '&format=' + format +
+    		 '&date=' + date + '&key=' + _PremiumApiKey + '&lang=' + language;
+    	callAPI(url, callback);
+}
+
+function callAPI(url,callback) {
+	request({
+	  uri: url,
+	  method: "GET"
+	}, function(error, response, body) {
+	  	callback(body);
+	});
+}
+
+/*
+ info à récupérer :
+	la température 		-> data.current_condition['0'].lang_fr
+	le temps 			-> data.current_condition['0'].temp_C
+	un icone de temps 	-> data.current_condition['0'].weatherIconUrl['0'].value
+*/
